50d59d89d77d6a8aec837be3e8be6f1e9e5b549a
Uczen <Uczen@DESKTOP-R1NK4H8>
2024-11-21 09:26:17 +0100

    Dodaj pliki projektów.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RGB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace RGB
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = SliderR.Value;
+            var green = SliderG.Value;
+            var blue = SliderB.Value;
+
+            var color = Color.FromRgb(red, green, blue);
+            Kwadrat.Fill = new SolidColorBrush(color);
+
+        }
+    }
+
+}
